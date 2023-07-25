efccab9709d512b3a97019613e9d466992045bf6
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-07-25 05:26:38 +0200

    fix WM MOVE linux bug

--- src/dd.c
+++ src/dd.c
@@ -716,7 +716,7 @@ HRESULT dd_SetDisplayMode(DWORD dwWidth, DWORD dwHeight, DWORD dwBPP, DWORD dwFl
         if (!g_ddraw->wine)
         {
             MSG msg; /* workaround for "Not Responding" window problem in cnc games */
-            real_PeekMessageA(&msg, g_ddraw->hwnd, 0, 0, PM_NOREMOVE);
+            real_PeekMessageA(&msg, g_ddraw->hwnd, 0, 0, PM_NOREMOVE | PM_QS_INPUT);
         }
 
         if (!border)
