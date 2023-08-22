f564fa840e0c649daf5ae37286937e4dc2c8185e
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-08-22 16:04:50 +0200

    improve logging

--- src/wndproc.c
+++ src/wndproc.c
@@ -24,13 +24,15 @@ LRESULT CALLBACK fake_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
         uMsg != WM_D3D9DEVICELOST)
     {
         TRACE(
-            "     uMsg = %s (%d), wParam = %08X (%d), lParam = %08X (%d)\n",
+            "     uMsg = %s (%d), wParam = %08X (%d), lParam = %08X (%d, LO=%d HI=%d)\n",
             dbg_mes_to_str(uMsg),
             uMsg,
             wParam,
             wParam,
             lParam,
-            lParam);
+            lParam,
+            (int)(short)LOWORD(lParam),
+            (int)(short)HIWORD(lParam));
     }
 #endif
 
@@ -181,6 +183,13 @@ LRESULT CALLBACK fake_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
     case WM_WINDOWPOSCHANGED:
     {
         WINDOWPOS* pos = (WINDOWPOS*)lParam;
+
+        /*
+        dbg_dump_swp_flags(pos->flags);
+        TRACE(
+            "     hwndInsertAfter=%p, x=%d, y=%d, cx=%d, cy=%d\n",
+            pos->hwndInsertAfter, pos->x, pos->y, pos->cx, pos->cy);
+        */
 
         if (g_ddraw->wine &&
             !g_ddraw->windowed &&
