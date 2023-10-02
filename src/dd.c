bbf529f6efc39c3929d6695371a8ee6efcf30232
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-02 02:19:21 +0200

    limit fps also with maxfps=0

--- src/dd.c
+++ src/dd.c
@@ -1189,7 +1189,9 @@ HRESULT dd_WaitForVerticalBlank(DWORD dwFlags, HANDLE hEvent)
 {
     if (g_config.maxgameticks == -2)
     {
-        BOOL open = !(g_config.maxfps < 0 || (g_config.vsync && g_config.maxfps >= g_ddraw->mode.dmDisplayFrequency));
+        BOOL open = 
+            !(g_config.maxfps < 0 || 
+                (g_config.vsync && (!g_config.maxfps || g_config.maxfps >= g_ddraw->mode.dmDisplayFrequency));
 
         if (fpsl_dwm_flush() || fpsl_wait_for_vblank(open))
             return DD_OK;
